c2a552e8ba52447b8c747a7179425fb8cec6585a
Molly Adamsson <adamssonmolly@gmail.com>
2023-04-20 18:20:27 +0000

    Added some more styling and rearanged the createpostform

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -128,7 +128,7 @@ function PostCreateForm() {
   return (
     <Form onSubmit={handleSubmit}>
       <Row>
-        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+        <Col className="py-2 p-0 p-md-2" md={5} lg={6}>
           <Container
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
